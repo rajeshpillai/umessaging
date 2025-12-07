58ef72b11c6e05d94dcabc04cd3a3608f75b3eaa
Rajesh Pillai <rajesh@algorisys.com>
2025-12-07 09:40:29 +0530

    Refactor umessagin lib

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const uWS = require('uWebSockets.js');
 const fs = require('fs');
 const path = require('path');
+const uMessage = require('./lib/uMessage');
 
-const users = new Map(); // socket -> { name, mobile }
-const groups = new Map(); // groupId -> Set<socket>
+const chat = new uMessage();
 
 const getContentType = (ext) => {
     switch (ext) {
@@ -23,106 +23,12 @@ const app = uWS.App().ws('/*', {
     idleTimeout: 60,
 
     open: (res, req, context) => {
-        /* On open we just let them connect */
         console.log('A WebSocket connected!');
+        chat.handleOpen(res);
     },
 
     message: (res, message, isBinary) => {
-        /* Parse the message */
-        try {
-            const buffer = Buffer.from(message);
-            const text = buffer.toString();
-            console.log('Received: ' + text);
-            const json = JSON.parse(text);
-
-            if (json.type === 'register') {
-                const { name, mobile } = json;
-                users.set(res, { name, mobile });
-                res.send(JSON.stringify({ type: 'registered', success: true }), false, true);
-                console.log(`User registered: ${name} (${mobile})`);
-
-                // Send existing groups to the new user
-                const groupList = Array.from(groups.keys());
-                res.send(JSON.stringify({ type: 'group_list', groups: groupList }), false, true);
-
-                // Send online users to the new user
-                const userList = [];
-                for (const [ws, u] of users.entries()) {
-                    if (ws !== res) userList.push(u);
-                }
-                res.send(JSON.stringify({ type: 'user_list', users: userList }), false, true);
-
-                // Broadcast user joined to ALL connected users (except self, usually, but uWS doesn't have "broadcast except")
-                // We'll iterate manually or just send to all, frontend can filter self if needed.
-                const userJoinedPayload = JSON.stringify({ type: 'user_joined', user: { name, mobile } });
-                users.forEach((u, ws) => {
-                    if (ws !== res) ws.send(userJoinedPayload, false, true);
-                });
-            }
-            else if (json.type === 'join_group') {
-                const { groupName } = json;
-                let isNew = false;
-                if (!groups.has(groupName)) {
-                    groups.set(groupName, new Set());
-                    isNew = true;
-                }
-                const group = groups.get(groupName);
-                group.add(res);
-
-                // Notify group
-                res.send(JSON.stringify({ type: 'joined_group', groupName }), false, true);
-                console.log(`User joined group: ${groupName}`);
-
-                if (isNew) {
-                    // Broadcast new group to ALL connected users
-                    const newGroupPayload = JSON.stringify({ type: 'group_created', groupName });
-                    users.forEach((userData, ws) => {
-                        ws.send(newGroupPayload, false, true);
-                    });
-                }
-            }
-            else if (json.type === 'message') {
-                const { to, content } = json;
-                if (groups.has(to)) {
-                    // It's a group
-                    const group = groups.get(to);
-                    const sender = users.get(res);
-                    const payload = JSON.stringify({
-                        type: 'message',
-                        from: sender ? sender.name : 'Anonymous',
-                        fromMobile: sender ? sender.mobile : '?',
-                        to: to, // group name
-                        content: content
-                    });
-                    group.forEach(ws => {
-                        ws.send(payload, false, true);
-                    });
-                } else {
-                    // It's a direct message (try to find by mobile)
-                    let targetSocket = null;
-                    for (const [ws, userData] of users.entries()) {
-                        if (userData.mobile === to) {
-                            targetSocket = ws;
-                            break;
-                        }
-                    }
-
-                    if (targetSocket) {
-                        const sender = users.get(res);
-                        targetSocket.send(JSON.stringify({
-                            type: 'message',
-                            from: sender ? sender.name : 'Anonymous',
-                            fromMobile: sender ? sender.mobile : '?',
-                            to: to, // receiver mobile
-                            content: content
-                        }), false, true);
-                    }
-                }
-            }
-
-        } catch (e) {
-            console.error('Error processing message:', e);
-        }
+        chat.handleMessage(res, message, isBinary);
     },
 
     drain: (res) => {
@@ -130,17 +36,7 @@ const app = uWS.App().ws('/*', {
     },
 
     close: (res, code, message) => {
-        console.log('WebSocket closed');
-        const user = users.get(res);
-        if (user) {
-            // Broadcast user left
-            const userLeftPayload = JSON.stringify({ type: 'user_left', mobile: user.mobile });
-            users.forEach((u, ws) => {
-                if (ws !== res) ws.send(userLeftPayload, false, true);
-            });
-        }
-        users.delete(res);
-        groups.forEach(group => group.delete(res));
+        chat.handleClose(res);
     }
 
 }).any('/*', (res, req) => {
